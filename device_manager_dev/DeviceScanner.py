f0bb435766fc6471683ca2441876562371fd3f3b
Max L <max@lrbg.de>
2021-04-21 12:20:15 +0200

    tty

--- device_manager_dev/DeviceScanner.py
+++ device_manager_dev/DeviceScanner.py
@@ -10,7 +10,7 @@ class DeviceScanner():
         #alles aus dev holen
         for (dirpath, dirnames, filenames) in walk("/dev/"):
             #USB verbindungen filtern
-            filtered_filenames = [filename for filename in filenames if filename.startswith("ttyS")]
+            filtered_filenames = [filename for filename in filenames if filename.startswith("tty")]
 
             #USB verbindungen durchgehen
             for filename in filtered_filenames:
