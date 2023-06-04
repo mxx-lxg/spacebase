6f3045ae75d1cd437a0d9cebac521664951fdbdb
Max L <7644014+mxx-lxg@users.noreply.github.com>
2023-06-04 14:39:58 +0200

    config flow

--- system/spacebase.py
+++ system/spacebase.py
@@ -47,7 +47,29 @@ if not os.path.exists(PIPE_PATH):
 #Konfiguartion laden oder erstellen
 
 if not exists('/home/pi/spacebase/system/spacebase.conf'):
-    print("No config file found. Exiting...")
+    print("No config file found. Writing new config and exiting...")
+    
+    newConfig = configparser.ConfigParser()
+    newConfig.add_section('windows')
+    newConfig.set('windows', 'stage_1', '22')
+    newConfig.set('windows', 'stage_2', '24')
+    newConfig.set('windows', 'stage_3', '26')
+    newConfig.set('windows', 'stage_4', '28')
+    newConfig.set('windows', 'check_interval', '300')
+    newConfig.add_section('climate')
+    newConfig.set('climate', 'log_interval', '300')
+    newConfig.set('climate', 'heater_threshold', '1')
+    newConfig.set('climate', 'heater_stop', '3')
+    newConfig.add_section('irrigation')
+    newConfig.set('irrigation', 'left_threshold', '15')
+    newConfig.set('irrigation', 'right_threshold', '15')
+    newConfig.add_section('mqtt')
+    newConfig.set('mqtt', 'broker', 'smart.home')
+    newConfig.set('mqtt', 'port', '1883')
+    newConfig.set('mqtt', 'client_id', '69420')
+    newConfig.set('mqtt', 'user', 'max')
+    newConfig.set('mqtt', 'pw', 'public')
+
     sys.exit()
 
 
