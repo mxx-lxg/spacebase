328d8437e643e1cd9e7ca097a2e770872cca3336
Max L <>
2024-04-07 16:38:42 +0200

    structure rework

---
+++ system/Habitat.py
@@ -0,0 +1,158 @@
+import logging
+import schedule
+import time
+import datetime
+
+from RelayController import Heater, Pump
+from DeviceScanner import DeviceScanner
+from Windows import Windows
+from Irrigation import Irrigation
+from Environment import Environment
+from Moisture import Moisture
+from MqttClient import MqttClient
+
+class Habitat():
+    hibernation_mode = False
+    heating_in_progress = False
+    logger = logging.getLogger(__name__)
+    config  = None
+    heaterStartVal = 0
+    heaterStopVal = 2
+    irrigationVal = 20
+    mqttClient = None
+
+    heater = Heater()
+    pump = Pump()
+    windows = None
+    irrigation = None
+    environment = None
+    moisture = [] 
+
+    def __init__(self, config):
+        self.config = config
+                
+        self.heaterStartVal = float(config['climate']['heater_threshold'])
+        self.heaterStopVal = float(config['climate']['heater_stop'])
+        self.irrigationVal = float(config['irrigation']['left_threshold'])
+
+        #MQTT Client
+        self.mqttClient = MqttClient(config['mqtt'])
+
+        self.initialize_devices()
+        self.create_jobs()
+
+
+    def initialize_devices(self):
+        #Geräte suchen und initialisieren
+        scannedDevices = DeviceScanner.findDevices()
+
+        print("device init")
+        for path in scannedDevices:
+            device = scannedDevices[path]
+            if device == "windows":
+                self.windows = Windows(path, self.mqttClient)
+            if device == "environment":
+                self.environment = Environment(path)
+                self.environment.init()
+            if device == "water":
+                self.irrigation = Irrigation(path)
+                self.irrigation.init()
+            if device == "moisture":
+                self.moisture.append(Moisture(path))
+                self.moisture[-1].init()
+
+    def create_jobs(self):
+        #Jobs anlegen
+        if self.environment:
+            schedule.every(int(self.config['climate']['log_interval'])).seconds.do(self.environmentReport)
+            schedule.every(1).seconds.do(self.frostProtection)
+
+        if self.irrigation:
+            self.irrigation.getRainWaterLevel()
+            schedule.every(int(self.config['climate']['log_interval'])).seconds.do(self.irrigationReport)
+            schedule.every().day.at("11:30").do(self.irrigateAll)
+
+        if self.moisture:
+            schedule.every(int(self.config['climate']['log_interval'])).seconds.do(self.moistureReport)
+
+        if self.windows:
+            schedule.every(int(self.config['windows']['check_interval'])).seconds.do(self.adjustWindows)
+            
+            
+    def environmentReport(self):
+        #Klimadaten speichern
+        self.mqttClient.logEnvironment(self.environment.lastTemperature, self.environment.lastHumidity)
+
+
+    def irrigationReport(self):
+        self.irrigation.getRainWaterLevel()
+        time.sleep(1)
+        self.mqttClient.logIrrigation(self.irrigation.rainWaterLevel)
+
+    def moistureReport(self):
+    #Bodenfeuchtigkeit speichern
+        for patch in self.moisture:
+            print("{0} | patch {1} moisture: {2} % - logged \r".format(
+                datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                patch.patchId,
+                patch.lastMoisture
+            ))
+        #TODO moisture logging
+        #Bewässerung
+        #1 mal morgens
+            #Bewässerung unabhängig von Feuchtigkeit
+        #2 mal täglich
+            #Liste der beete durchgehen und für jedes Moisture Sensor abfragen
+            #Wenn unter Grenze Bewässerung aktivieren
+
+    def adjustWindows(self):
+        #Fenster einstellen
+        if self.environment.lastTemperature >= float(self.config['windows']['stage_4']): 
+            self.windows.setToStage(4)
+        elif self.environment.lastTemperature >= float(self.config['windows']['stage_3']): 
+            self.windows.setToStage(3)
+        elif self.environment.lastTemperature >= float(self.config['windows']['stage_2']): 
+            self.windows.setToStage(2)
+        elif self.environment.lastTemperature >= float(self.config['windows']['stage_1']): 
+            self.windows.setToStage(1)
+        else:
+            self.windows.setToStage(0) 
+
+    def frostProtection(self):
+        #Frostschutz Heizung
+        if not self.hibernation_mode:
+            if not self.heating_in_progress:
+                if self.environment.lastTemperature <= self.heaterStartVal: 
+                    self.heater.heaterOn()
+                    self.heating_in_progress = True
+                    self.logger.warning("below frost threshold: " + str(self.environment.lastTemperature))
+                    print("starting defrost")
+            else: 
+                if self.environment.lastTemperature >= self.heaterStopVal: 
+                    self.heater.heaterOff()
+                    self.heating_in_progress = False
+                    print("stopping defrost")
+
+    def irrigateAll(self):
+        self.irrigation.getRainWaterLevel()
+        time.sleep(1)
+        if self.irrigation.rainWaterLevel > 30 and not self.hibernation_mode:
+            startLevel = self.irrigation.rainWaterLevel
+            failSafeCounter = 400
+            amount = 10
+            print("starting irrigation...")
+            self.pump.pumpOn()
+
+            for tick in range(failSafeCounter):
+                self.irrigation.getRainWaterLevel()
+                time.sleep(1)
+                failSafeCounter = failSafeCounter - 1
+                if self.irrigation.rainWaterLevel <= startLevel - amount:
+                    break
+
+            self.pump.pumpOff()
+            print("irrigation complete ({0})".format(failSafeCounter))
+        else:
+            
+            print("not enough water! ({0})".format(self.irrigation.rainWaterLevel))
+    
---
+++ system/spacebase_old.py
@@ -0,0 +1,415 @@
+import configparser
+import datetime
+import logging
+import os
+import sys
+import threading
+import time
+from os.path import exists
+
+import keyboard
+import schedule
+from Dashboard import Dashboard
+from DataLogger import DataLogger
+from DeviceScanner import DeviceScanner
+from Environment import Environment
+from Irrigation import Irrigation
+from Moisture import Moisture
+from MqttClient import MqttClient
+from RelayController import Heater, Pump
+from Windows import Windows
+from UserInterface import UserInterface
+
+#hiberantion for general winter idle
+hibernationMode = False
+
+#log setup
+# set up logging to file
+print("working directory: " + os.getcwd())
+logging.basicConfig(
+    filename= os.getcwd() + '/spacebase.log',
+    level=logging.INFO, 
+    format= '[%(asctime)s] %(name)s: %(levelname)s - %(message)s',
+    datefmt='%d.%m.%Y %H:%M:%S'
+)
+# set up logging to console
+console = logging.StreamHandler()
+console.setLevel(logging.ERROR)
+# set a format which is simpler for console use
+formatter = logging.Formatter('[%(asctime)s] %(name)s: %(levelname)s %(message)s')
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+logger = logging.getLogger(__name__)
+
+logger.info("startup")
+
+#monitoring pipe
+#TODO piping in einem Datenformat für externes Dashboard programm
+PIPE_PATH = "/tmp/greenhouse_pipe"
+if not os.path.exists(PIPE_PATH):
+    os.mkfifo(PIPE_PATH)
+
+#Konfiguartion laden oder erstellen
+
+if not exists(os.getcwd() + '/spacebase.conf'):
+    print("No config file found. Writing new config and exiting...")
+    
+    newConfig = configparser.ConfigParser()
+    newConfig.add_section('windows')
+    newConfig.set('windows', 'stage_1', '22')
+    newConfig.set('windows', 'stage_2', '24')
+    newConfig.set('windows', 'stage_3', '26')
+    newConfig.set('windows', 'stage_4', '28')
+    newConfig.set('windows', 'check_interval', '300')
+    newConfig.add_section('climate')
+    newConfig.set('climate', 'log_interval', '300')
+    newConfig.set('climate', 'heater_threshold', '1')
+    newConfig.set('climate', 'heater_stop', '3')
+    newConfig.add_section('irrigation')
+    newConfig.set('irrigation', 'left_threshold', '15')
+    newConfig.set('irrigation', 'right_threshold', '15')
+    newConfig.add_section('mqtt')
+    newConfig.set('mqtt', 'broker', 'smart.home')
+    newConfig.set('mqtt', 'port', '1883')
+    newConfig.set('mqtt', 'client_id', '69420')
+    newConfig.set('mqtt', 'user', 'max')
+    newConfig.set('mqtt', 'pw', 'public')
+    with open(os.getcwd() + '/spacebase.conf', 'x') as configFile:
+        newConfig.write(configFile)
+        configFile.close()
+        sys.exit()
+
+
+print("loading config")
+config = configparser.ConfigParser()
+config.read(os.getcwd() + '/spacebase.conf')
+
+
+
+heaterStartVal = float(config['climate']['heater_threshold'])
+heaterStopVal = float(config['climate']['heater_stop'])
+
+irrigationVal = float(config['irrigation']['left_threshold'])
+
+stage1 = float(config['windows']['stage_1'])    #TODO in objekt
+stage2 = float(config['windows']['stage_2'])
+stage3 = float(config['windows']['stage_3'])
+stage4 = float(config['windows']['stage_4'])
+
+#MQTT Client
+mqttClient = MqttClient(config['mqtt'])
+
+
+
+#Logger
+print("initialising data logs")
+dataLogger = DataLogger(mqttClient)
+
+#GPIO
+heater = Heater()
+pump = Pump()
+
+#keypresses
+print("keyboard setup")
+keyboard.add_hotkey('1', lambda: windows.forceClosed())
+keyboard.add_hotkey('2', lambda: windows.forceOpened())
+keyboard.add_hotkey('3', lambda: windows.unforce())
+keyboard.add_hotkey('4', lambda: windows.reset())
+keyboard.add_hotkey('6', lambda: toggleHibernation())
+keyboard.add_hotkey('7', lambda: heater.heaterToggle())
+keyboard.add_hotkey('8', lambda: pump.pumpToggle())
+
+#Geräte suchen und initialisieren
+scannedDevices = DeviceScanner.findDevices()
+windows = None
+environment = None
+irrigation = None
+moisture = [] 
+
+#UI
+#dashboard = Dashboard()
+
+print("device init")
+for path in scannedDevices:
+    device = scannedDevices[path]
+    if device == "windows":
+        windows = Windows(path, mqttClient)
+    if device == "environment":
+        environment = Environment(path)
+        environment.init()
+    if device == "water":
+        irrigation = Irrigation(path)
+        irrigation.init()
+    if device == "moisture":
+        moisture.append(Moisture(path))
+        moisture[-1].init()
+
+#Sonnenauf- und untergang
+sunrise = 0
+sunset = 0
+
+#UI
+userInterface = UserInterface(windows, irrigation, environment, mqttClient, hibernationMode, heater, pump)
+
+#startup end
+logger.info("startup complete")
+
+
+print("\n getting down to business...")
+
+if mqttClient: mqttClient.publish("STATE", "hello")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#main Loop
+heatingInProgress = False
+
+def toggleHibernation():
+    hibernationMode = not hibernationMode
+
+def listCommands():
+    print("help................help")
+    print("status..............system status")
+    print("water...............run irrigation cycle")
+    print("wreset..............reset windows")
+    print("wset [stage]........set windows")
+    print("listjobs............list scheduled jobs")
+    print("hibernate [on/off]..activate hibernation")
+
+#system status
+def systemStatus():
+    global windows
+    global environment
+    global irrigation
+    global moisture
+    global mqttClient
+    global hibernationMode
+    global heaterStartVal
+    global heaterStopVal
+    global config
+    global stage1
+    global stage2
+    global stage3
+    global stage4
+
+    #devices
+    print("\n********* devices *********")
+    if windows:
+        print("* window actuators")
+    if environment:
+        print("* environmental sensor-package")
+    if irrigation:
+        print("* irrigation and water management")
+    if moisture:
+        print("* soil-moisture sensors:")
+        for patch in moisture:
+            print("    " + patch.patchId)
+
+    # system info
+    print("\n********* system *********")
+    if mqttClient.connected:
+        print("* connected to MQTT broker")
+    else: 
+        print("! not connected to MQTT broker")
+    if hibernationMode:
+        print("! hibernation mode is ON")
+    else:
+        print("* hibernation mode is OFF")
+
+
+    #config info
+    print("\n********* configuration *********")
+    print("\n* Heater")
+    print("   Start: " + str(heaterStartVal) + "°C | Stop: " + str(heaterStopVal) + "°C")
+    print("\n* Windows")
+    print("   check interval (s): " + config['windows']['check_interval'])
+    print("   thresholds")
+    print("   1: " + str(stage1) + "°C | 2: " + str(stage2) + "°C | 3: " + str(stage3) + "°C | 4: " + str(stage4) + "°C")
+
+
+
+def environmentReport():
+    #Klimadaten speichern
+    global environment
+    global dataLogger
+    print("{0} | temperature: {1} °C | humidity: {2} % - logged \r".format(
+        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        environment.lastTemperature,
+        environment.lastHumidity
+    ))
+    dataLogger.logEnvironment(environment.lastTemperature, environment.lastHumidity)
+
+def irrigationReport():
+    global irrigation
+    global dataLogger
+    irrigation.getRainWaterLevel()
+    time.sleep(1)
+    print("{0} | rainwater level: {1} % - logged \r".format(
+        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        irrigation.rainWaterLevel
+    ))
+    dataLogger.logIrrigation(irrigation.rainWaterLevel)
+
+def irrigateAll():
+    global irrigation
+    global pump
+    global hibernationMode
+
+    irrigation.getRainWaterLevel()
+    time.sleep(1)
+    if irrigation.rainWaterLevel > 30 and not hibernationMode:
+        startLevel = irrigation.rainWaterLevel
+        failSafeCounter = 400
+        amount = 10
+        print("starting irrigation...")
+        pump.pumpOn()
+
+        with alive_bar(manual=True, stats=False, title="water level") as bar:
+            for tick in range(failSafeCounter):
+                irrigation.getRainWaterLevel()
+                time.sleep(1)
+                failSafeCounter = failSafeCounter - 1
+                bar(irrigation.rainWaterLevel / 100)
+                if irrigation.rainWaterLevel <= startLevel - amount:
+                    break
+
+        pump.pumpOff()
+        print("irrigation complete ({0})".format(failSafeCounter))
+    else:
+        
+        print("not enough water! ({0})".format(irrigation.rainWaterLevel))
+
+def moistureReport():
+    #Bodenfeuchtigkeit speichern
+    for patch in moisture:
+        print("{0} | patch {1} moisture: {2} % - logged \r".format(
+            datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            patch.patchId,
+            patch.lastMoisture
+        ))
+    #TODO moisture logging
+    #Bewässerung
+    #1 mal morgens
+        #Bewässerung unabhängig von Feuchtigkeit
+    #2 mal täglich
+        #Liste der beete durchgehen und für jedes Moisture Sensor abfragen
+        #Wenn unter Grenze Bewässerung aktivieren
+
+def adjustWindows():
+    #Fenster einstellen
+    global environment
+    global windows
+    if environment.lastTemperature >= stage4: 
+        windows.setToStage(4)
+    elif environment.lastTemperature >= stage3: 
+        windows.setToStage(3)
+    elif environment.lastTemperature >= stage2: 
+        windows.setToStage(2)
+    elif environment.lastTemperature >= stage1: 
+        windows.setToStage(1)
+    else:
+        windows.setToStage(0)
+
+
+#frost protection
+def frostProtection():
+    #Frostschutz Heizung
+    global heatingInProgress
+    global heaterStartVal
+    global heater
+    global logger
+    global environment
+    global hibernationMode
+
+    if not hibernationMode:
+        if not heatingInProgress:
+            if environment.lastTemperature <= heaterStartVal: 
+                heater.heaterOn()
+                heatingInProgress = True
+                logger.warning("below frost threshold: " + str(environment.lastTemperature))
+                print("starting defrost")
+        else: 
+            if environment.lastTemperature >= heaterStopVal: 
+                heater.heaterOff()
+                heatingInProgress = False
+                print("stopping defrost")
+
+
+
+#Jobs anlegen
+if environment:
+    schedule.every(int(config['climate']['log_interval'])).seconds.do(environmentReport)
+    schedule.every(1).seconds.do(frostProtection)
+
+if irrigation:
+    irrigation.getRainWaterLevel()
+    schedule.every(int(config['climate']['log_interval'])).seconds.do(irrigationReport)
+    schedule.every().day.at("11:30").do(irrigateAll)
+
+if moisture:
+    schedule.every(int(config['climate']['log_interval'])).seconds.do(moistureReport)
+
+if windows:
+    schedule.every(int(config['windows']['check_interval'])).seconds.do(adjustWindows)
+
+
+#terminal commands
+def commandHandlerLoop():
+    global windows
+    global hibernationMode
+    global schedule
+
+
+    while True:
+        inputString = input('>')
+
+        args = inputString.split(' ')
+        command = args[0]
+
+        if command == "help":
+            listCommands()
+        if command == "status":
+            systemStatus()
+        if command == "water":
+            irrigateAll()
+
+        if command == "wreset":
+            windows.reset()
+        if command == "wset":
+            windows.setToStage(int(args[1]))
+
+        if command == "hibernate":
+            if args[1] == "on":
+                hibernationMode = True
+                print("hibernation mode activated ({0})".format(hibernationMode))
+            elif args[1] == "off":
+                hibernationMode = False
+                print("hibernation mode deactivated ({0})".format(hibernationMode))
+
+        if command == "listjobs":
+            jobs = schedule.get_jobs()
+            print("active jobs")
+            print(*jobs, sep = "\n")
+
+
+
+commandHandler = threading.Thread(target=commandHandlerLoop, args=())
+commandHandler.start()
+
+
+#main job loop
+while True:
+    schedule.run_pending()
+    time.sleep(1)
